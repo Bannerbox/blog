6d3b8b318b89c7ecea64cc1a365f6d83aca93f59
David Le <davidkle@gmail.com>
2023-02-09 08:50:43 -0800

    Generate all metadata

--- scripts/generate-index.ts
+++ scripts/generate-index.ts
@@ -3,8 +3,7 @@ import path from 'path';
 import Markdoc from '@markdoc/markdoc';
 import yaml from 'js-yaml';
 
-const dirPath = path.join('./pages', 'engineering');
-const files = fs.readdirSync(dirPath);
+type CATEGORY_FOLDER_TYPES = 'engineering' | 'product' | 'startup-journey';
 
 type PostMetadata = {
   title: string;
@@ -12,19 +11,60 @@ type PostMetadata = {
   image: string;
   date: string;
   author: string;
+
+  // Extra info
+  link: string;
+  category: string;
 };
 
-const postMetadata: Array<PostMetadata> = files.map((fileName) => {
-  const filePath = path.join('./pages', 'engineering', fileName);
-  const file = fs.readFileSync(filePath, 'utf-8');
-  const ast = Markdoc.parse(file);
+const getCategoryMetadata = (category: CATEGORY_FOLDER_TYPES): Array<PostMetadata> => {
+  // Read the directory
+  const dirPath = path.join('./pages', category);
+  const files = fs.readdirSync(dirPath);
 
-  const frontmatter = ast.attributes.frontmatter;
-  const data = yaml.load(frontmatter) as PostMetadata;
-  return data;
-});
+  // Read the file content individually
+  const postMetadata: Array<PostMetadata> = files.map((fileName) => {
+    const filePath = path.join('./pages', category, fileName);
+    const file = fs.readFileSync(filePath, 'utf-8');
 
-let dataToWrite = JSON.stringify(postMetadata);
-fs.writeFileSync(path.join('./public/blog-posts.json'), dataToWrite);
+    // Parse the data
+    const ast = Markdoc.parse(file);
+    const frontmatter = ast.attributes.frontmatter;
+    const data = yaml.load(frontmatter) as PostMetadata;
+
+    // Massage the data
+    data.link = path.join(category, fileName.split('.')[0]);
+    data.category = category;
+    return data;
+  });
+
+  // Newest ranked highest first
+  const sortedMetadata = postMetadata.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    if (dateA.getTime() < dateB.getTime()) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  return sortedMetadata;
+};
+
+const engineeringMetadata = getCategoryMetadata('engineering');
+const productMetadata = getCategoryMetadata('product');
+const startupJourneyMetadata = getCategoryMetadata('startup-journey');
+
+const allMetadata: Record<CATEGORY_FOLDER_TYPES, Array<PostMetadata>> = {
+  engineering: engineeringMetadata,
+  product: productMetadata,
+  'startup-journey': startupJourneyMetadata,
+};
+
+console.log(allMetadata);
+
+let dataToWrite = JSON.stringify(allMetadata);
+fs.writeFileSync(path.join('./public/all-posts-metadata.json'), dataToWrite);
 
 export {};
