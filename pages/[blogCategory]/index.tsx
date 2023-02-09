d89ef6065986a5a85832992da3d60ab00fc9bd78
David Le <davidkle@gmail.com>
2023-02-09 14:31:57 -0800

    Static props

--- pages/[blogCategory]/index.tsx
+++ pages/[blogCategory]/index.tsx
@@ -1,11 +1,25 @@
 import { useRouter } from 'next/router';
 import TopicList from 'components/topic-picker/TopicList';
 import { CATEGORY_TYPE } from 'types';
+import { GetStaticProps } from 'next';
 
-const Topic = () => {
-  const router = useRouter();
-  const { blogCategory } = router.query;
-  return <TopicList category={blogCategory as CATEGORY_TYPE} />;
+type Props = {
+  category: CATEGORY_TYPE;
+};
+
+const Topic = ({ category }: Props) => {
+  //   const router = useRouter();
+  //   const { blogCategory } = router.query;
+  return <TopicList category={category} />;
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const blogCategory = context.params?.blogCategory;
+  return {
+    props: {
+      category: blogCategory,
+    },
+  };
 };
 
 export default Topic;
