4242592017d564b124f3613494b0b47352289e21
David Le <davidkle@gmail.com>
2023-02-28 08:32:17 -0800

    Debug

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const withMarkdoc = require('@markdoc/next.js');
 
-module.exports = withMarkdoc({ mode: 'static' })({
+// module.exports = withMarkdoc({ mode: 'static' })({
+module.exports = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdoc'],
   trailingSlash: false,
   basePath: '/blog'
-}); 
+}; 
