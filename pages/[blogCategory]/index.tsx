9f041f6ea5edfc24c8fefe3358a51cbe377989fa
David Le <davidkle@gmail.com>
2023-03-02 08:00:18 -0800

    Debug

--- pages/[blogCategory]/index.tsx
+++ pages/[blogCategory]/index.tsx
@@ -24,8 +24,8 @@ export const getStaticProps: GetStaticProps = async (context) => {
 export async function getStaticPaths() {
   return {
     paths: [
-      // { params: { blogCategory: 'engineering' } },
       { params: { blogCategory: 'product' } },
+      // { params: { blogCategory: 'engineering' } },
       // { params: { blogCategory: 'startup-journey' } },
     ],
     // Next will pre-render only these paths at build time other routes will 404.
